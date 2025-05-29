1d6963e086218ca76a0372e4744459b3733a036e
levith0304 <157609541+levith0304@users.noreply.github.com>
2025-05-29 12:15:38 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,85 +1,31 @@
-window.onload = init;
-var petalos;
-var boton_jugar;
-var valores;
+document.addEventListener("DOMContentLoaded", function() {
+    const audio = new Audio("love.mp3"); 
+    
+    const playAudio = () => {
+        audio.play().catch(error => {
+            console.log("Reproducción automática bloqueada, esperando interacción del usuario.");
+        });
+    };
+    
+    playAudio();
+    
+    document.body.addEventListener("click", () => {
+        audio.play();
+    }, { once: true });
+});
 
-function init(){
-	petalos = document.getElementsByClassName("petalo");
-	asignarEvento();
-  boton_jugar = document.getElementById("botonJugar");
-  boton_jugar.addEventListener("click",abrirJuego);
-}
-function asignarValores(){
-  var numero_aleatorio = Math.round(Math.random()*10);
-  var moneda = numero_aleatorio > 5;
-  valores = [];
-  for(var i=0;i<petalos.length;i++)
-	{
-    valores[i] = moneda;
-    moneda = !moneda;
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'F12') {
+    e.preventDefault();
   }
-}
-function abrirJuego(){
-  var ventana_actual = document.getElementById("intro");
-  var ventana_juego = document.getElementById("juego");
-  ventana_actual.className = "hidden";
-  ventana_juego.className = "animated fadeIn";
-  asignarValores();
-}
-function asignarEvento(){
-	for(var i=0;i<petalos.length;i++)
-	{
-		petalos[i].addEventListener("click",animar);
-	}
-}
-function colorAleatorio(){
-  var r = Math.round(Math.random()*255);
-  var g = Math.round(Math.random()*255);
-  var b = Math.round(Math.random()*255);
-  return "rgb("+r+","+g+","+b+")";
-}
-function animar(event){
-  var id_petalo = event.target.id;
-  var petalo = document.getElementById(""+id_petalo);
-  //petalo.style.backgroundColor = colorAleatorio(); 
-  petalo.className = "petalo animated fadeOutDown";
-  validarJugada(id_petalo);
-}
-
-function validarJugada(petalo){
-  var indice = parseInt(petalo.substr(1));
-  var str;
-  
-  if(valores[indice-1])
-  {
-    str = "No quiero";
+  if (e.ctrlKey && e.shiftKey && e.key === 'I') {
+    e.preventDefault();
   }
-  else
-  {
-    str = "Si quiero";    
+  if (e.ctrlKey && e.key === 'u') {
+    e.preventDefault();
   }
-  //alert(mensaje);
-  texto_mensaje.innerHTML = str;
-  mensaje.className = "mostrar animated fadeIn";
-  setTimeout(()=>{
-    mensaje.className = "hidden";
-  },1000);
-}
-/*window.onload = init;
-var petalos;
-var contador;
-
-function init(){
-	var boton = document.getElementById("boton");
-	boton.addEventListener("click",animar);
-	contador = 0;
-	petalos =         document.getElementsByClassName("petalo");
-}
-
-function animar(){
-	if(contador+1<=petalos.length)
-	{
-		petalos[contador].className = "petalo animated bounce infinite";
-		contador = contador+1;
-	}
-}*/
+});
